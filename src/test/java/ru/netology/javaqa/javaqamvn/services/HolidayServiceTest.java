51fa2c76032b9c5f64d20632a4fd405a6b61a269
ShibaevFntk <ShibaevFntk@gmail.com>
2025-05-26 00:34:20 +0300

    HW_CYCLES_02

--- src/test/java/ru/netology/javaqa/javaqamvn/services/HolidayServiceTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/HolidayServiceTest.java
@@ -1,27 +1,19 @@
 package ru.netology.javaqa.javaqamvn.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class HolidayServiceTest {
 
-    @Test
-    public void testHolidayCount1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/HolidayServiceTest.csv")
+
+    public void testHolidayCount() {
 
         ServiceHoliday service = new ServiceHoliday();
         int expected = 2;
         int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    public void testHolidayCount2() {
-
-        ServiceHoliday service = new ServiceHoliday();
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
 
         Assertions.assertEquals(expected, actual);
 
